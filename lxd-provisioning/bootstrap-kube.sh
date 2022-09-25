4fc647ad02a7239132c3a77fdbde11176f70b4ad
Prasannjeet Singh <prasannjeet.singh@ist.com>
2022-09-26 00:35:48 +0200

    flannel fix

--- lxd-provisioning/bootstrap-kube.sh
+++ lxd-provisioning/bootstrap-kube.sh
@@ -35,6 +35,9 @@ echo 'FLANNEL_NETWORK=10.244.0.0/16
 FLANNEL_SUBNET=10.244.0.1/24
 FLANNEL_MTU=1450
 FLANNEL_IPMASQ=true' > /run/flannel/subnet.env
+
+
+sed -i '1s/^/[Unit]\nDescription=Kubernetes\nAfter=syslog.target\nAfter=network.target\n/' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 # Tweaks end
 
 systemctl restart kubelet
