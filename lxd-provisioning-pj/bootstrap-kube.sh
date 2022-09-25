9cc22c3f7dc86712682886d3593ff309a6e9e461
Prasannjeet Singh <prasannjeet.singh@ist.com>
2022-09-25 21:50:02 +0200

    moved flannel subnet file above

--- lxd-provisioning-pj/bootstrap-kube.sh
+++ lxd-provisioning-pj/bootstrap-kube.sh
@@ -29,6 +29,11 @@ echo overlay >> /etc/modules
 
 echo '#!/bin/sh -e
 mount --make-rshared /' > /etc/rc.local
+
+echo 'FLANNEL_NETWORK=10.244.0.0/16
+FLANNEL_SUBNET=10.244.0.1/24
+FLANNEL_MTU=1450
+FLANNEL_IPMASQ=true' > /run/flannel/subnet.env
 # Tweaks end
 
 systemctl restart kubelet
