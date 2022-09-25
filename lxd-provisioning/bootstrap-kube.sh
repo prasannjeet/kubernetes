ee7be6b1c286d26c5199576c757ed6169094177a
Prasannjeet Singh <prasannjeet.singh@ist.com>
2022-09-25 20:31:59 +0200

    added linux-image package in kubelx file

--- lxd-provisioning/bootstrap-kube.sh
+++ lxd-provisioning/bootstrap-kube.sh
@@ -5,7 +5,6 @@
 
 echo "[TASK 0] Install essential packages"
 apt install -qq -y net-tools curl ssh software-properties-common >/dev/null 2>&1
-apt-get install -qq linux-image-$(uname -r)
 
 echo "[TASK 1] Install containerd runtime"
 apt update -qq >/dev/null 2>&1
