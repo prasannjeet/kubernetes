8a508102dee151404cf9e9a2f702a7c837a3ce8a
Prasannjeet Singh <prasannjeet.singh@ist.com>
2022-09-26 19:54:20 +0200

    removed silent commands

--- lxd-provisioning-pj/bootstrap-kube.sh
+++ lxd-provisioning-pj/bootstrap-kube.sh
@@ -4,23 +4,23 @@
 # For other versions of Ubuntu, you might need some tweaking
 
 echo "[TASK 0] Install essential packages"
-apt install -qq -y net-tools curl ssh software-properties-common >/dev/null 2>&1
-apt-get install -qq -y linux-image-$(uname -r) >/dev/null 2>&1
+apt install -qq -y net-tools curl ssh software-properties-common 
+apt-get install -qq -y linux-image-$(uname -r) 
 
 echo "[TASK 1] Install containerd runtime"
-apt update -qq >/dev/null 2>&1
-apt install -qq -y containerd apt-transport-https >/dev/null 2>&1
+apt update -qq 
+apt install -qq -y containerd apt-transport-https 
 mkdir /etc/containerd
 containerd config default > /etc/containerd/config.toml
 systemctl restart containerd
-systemctl enable containerd >/dev/null 2>&1
+systemctl enable containerd 
 
 echo "[TASK 2] Add apt repo for kubernetes"
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" 
 
 echo "[TASK 3] Install Kubernetes components (kubeadm, kubelet and kubectl)"
-apt install -qq -y kubeadm=1.25.2-00 kubelet=1.25.2-00 kubectl=1.25.2-00 >/dev/null 2>&1
+apt install -qq -y kubeadm=1.25.2-00 kubelet=1.25.2-00 kubectl=1.25.2-00 
 echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/default/kubelet
 
 # Some tweaks to ensure vm works
@@ -49,8 +49,8 @@ echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 systemctl reload sshd
 
 echo "[TASK 5] Set root password"
-echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo -e "kubeadmin\nkubeadmin" | passwd root 
 echo "export TERM=xterm" >> /etc/bash.bashrc
 
 echo "[TASK 6] Install additional packages"
-apt install -qq -y net-tools >/dev/null 2>&1
+apt install -qq -y net-tools 
