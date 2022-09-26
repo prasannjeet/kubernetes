3552b503af42622a6e2066c4c2cff9b550f26ca8
Prasannjeet Singh <prasannjeet.singh@ist.com>
2022-09-26 03:07:46 +0200

    modified creation a bit in pj folder

--- lxd-provisioning-pj/bootstrap-kube.sh
+++ lxd-provisioning-pj/bootstrap-kube.sh
@@ -20,7 +20,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
 
 echo "[TASK 3] Install Kubernetes components (kubeadm, kubelet and kubectl)"
-apt install -qq -y kubeadm=1.22.0-00 kubelet=1.22.0-00 kubectl=1.22.0-00 >/dev/null 2>&1
+apt install -qq -y kubeadm=1.25.2-00 kubelet=1.25.2-00 kubectl=1.25.2-00 >/dev/null 2>&1
 echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/default/kubelet
 
 # Some tweaks to ensure vm works
@@ -29,11 +29,16 @@ echo overlay >> /etc/modules
 
 echo '#!/bin/sh -e
 mount --make-rshared /' > /etc/rc.local
-
+mkdir /run/flannel
+touch /run/flannel/subnet.env
 echo 'FLANNEL_NETWORK=10.244.0.0/16
 FLANNEL_SUBNET=10.244.0.1/24
 FLANNEL_MTU=1450
 FLANNEL_IPMASQ=true' > /run/flannel/subnet.env
+
+
+sed -i '1s/^/[Unit]\nDescription=Kubernetes\nAfter=syslog.target\nAfter=network.target\n/' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+systemctl daemon-reload
 # Tweaks end
 
 systemctl restart kubelet
@@ -49,41 +54,3 @@ echo "export TERM=xterm" >> /etc/bash.bashrc
 
 echo "[TASK 6] Install additional packages"
 apt install -qq -y net-tools >/dev/null 2>&1
-
-#######################################
-# To be executed only on master nodes #
-#######################################
-
-if [[ $(hostname) =~ .*master.* ]]
-then
-
-  echo "[TASK 7] Pull required containers"
-  kubeadm config images pull >/dev/null 2>&1
-
-  echo "[TASK 8] Initialize Kubernetes Cluster"
-  kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all >> /root/kubeinit.log 2>&1
-
-  echo "[TASK 9] Copy kube admin config to root user .kube directory"
-  mkdir /root/.kube
-  cp /etc/kubernetes/admin.conf /root/.kube/config  
-
-  echo "[TASK 10] Deploy Flannel network"
-  kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml > /dev/null 2>&1
-
-  echo "[TASK 11] Generate and save cluster join command to /joincluster.sh"
-  joinCommand=$(kubeadm token create --print-join-command 2>/dev/null) 
-  echo "$joinCommand --ignore-preflight-errors=all" > /joincluster.sh
-
-fi
-
-#######################################
-# To be executed only on worker nodes #
-#######################################
-
-if [[ $(hostname) =~ .*worker.* ]]
-then
-  echo "[TASK 7] Join node to Kubernetes Cluster"
-  apt install -qq -y sshpass >/dev/null 2>&1
-  sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.lxd:/joincluster.sh /joincluster.sh 2>/tmp/joincluster.log
-  bash /joincluster.sh >> /tmp/joincluster.log 2>&1
-fi
---
+++ lxd-provisioning-pj/configure-kube.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# This script has been tested on Ubuntu 20.04
+# For other versions of Ubuntu, you might need some tweaking
+
+#######################################
+# To be executed only on master nodes #
+#######################################
+
+if [[ $(hostname) =~ .*master.* ]]
+then
+
+  echo "[TASK 7] Pull required containers"
+  kubeadm config images pull >/dev/null 2>&1
+
+  echo "[TASK 8] Initialize Kubernetes Cluster"
+  kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all >> /root/kubeinit.log 2>&1
+
+  echo "[TASK 9] Copy kube admin config to root user .kube directory"
+  mkdir /root/.kube
+  cp /etc/kubernetes/admin.conf /root/.kube/config  
+
+  echo "[TASK 10] Deploy Flannel network"
+  # kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+  su ubuntu
+  sleep 35
+  kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+  echo "[TASK 11] Generate and save cluster join command to /joincluster.sh"
+  joinCommand=$(kubeadm token create --print-join-command 2>/dev/null) 
+  echo "$joinCommand --ignore-preflight-errors=all" > /joincluster.sh
+
+fi
+
+#######################################
+# To be executed only on worker nodes #
+#######################################
+
+if [[ $(hostname) =~ .*worker.* ]]
+then
+  echo "[TASK 7] Join node to Kubernetes Cluster"
+  apt install -qq -y sshpass >/dev/null 2>&1
+  sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.lxd:/joincluster.sh /joincluster.sh 2>/tmp/joincluster.log
+  bash /joincluster.sh >> /tmp/joincluster.log 2>&1
+fi
