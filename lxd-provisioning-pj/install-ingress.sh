e842e1ca785b531c49e4b50a8266cab7d027455d
Prasannjeet Singh <prasannjeet.singh@ist.com>
2022-10-01 05:56:27 +0200

    added ingress installation scripts

---
+++ lxd-provisioning-pj/install-ingress.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir /home/ubuntu/Documents && cd /home/ubuntu/Documents
+git clone https://github.com/nginxinc/kubernetes-ingress.git --branch v2.3.1
+cd kubernetes-ingress/deployments
+kubectl apply -f common/ns-and-sa.yaml
+kubectl apply -f rbac/rbac.yaml
+kubectl apply -f common/default-server-secret.yaml
+kubectl apply -f common/nginx-config.yaml
+kubectl apply -f common/ingress-class.yaml
+kubectl apply -f common/crds/k8s.nginx.org_virtualservers.yaml
+kubectl apply -f common/crds/k8s.nginx.org_virtualserverroutes.yaml
+kubectl apply -f common/crds/k8s.nginx.org_transportservers.yaml
+kubectl apply -f common/crds/k8s.nginx.org_policies.yaml
+kubectl apply -f common/crds/k8s.nginx.org_globalconfigurations.yaml
+kubectl apply -f daemon-set/nginx-ingress.yaml
