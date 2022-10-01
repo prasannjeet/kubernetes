c04025c5fa00a157ea95da1314a9dcbdc2231ff9
Prasannjeet Singh <prasannjeet.singh@ist.com>
2022-10-01 07:22:07 +0200

    removed scp command and replaced with lxc file push pull

---
+++ lxd-provisioning-pj/configure-worker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#######################################
+# To be executed only on worker nodes #
+#######################################
+
+if [[ $(hostname) =~ .*worker.* ]]
+then
+  echo "[TASK 7] Join node to Kubernetes Cluster"
+  apt install -qq -y sshpass >/dev/null 2>&1
+#   sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.lxd:/joincluster.sh /joincluster.sh 2>/tmp/joincluster.log
+  bash /joincluster.sh >> /tmp/joincluster.log 2>&1
+fi
