6a33512a77a2d3f9ada1a369e5c5dd9fb141bf65
Prasannjeet Singh <prasannjeet.singh@ist.com>
2022-10-05 01:02:20 +0200

    rancher 2.7 rc install

--- lxd-provisioning-pj/install-rancher.sh
+++ lxd-provisioning-pj/install-rancher.sh
@@ -2,10 +2,13 @@
 
 snap install helm --classic
 helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 kubectl create namespace cattle-system
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.crds.yaml
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.7.1
-helm install rancher rancher-stable/rancher --namespace cattle-system --set hostname=rancher.logicalguy.com --set bootstrapPassword=admin
+helm install rancher rancher/rancher:v2.7.0-rc3 --namespace cattle-system --set hostname=rancher.logicalguy.com --set bootstrapPassword=admin
+# rancher/rancher:v2.7.0-rc3
+# rancher-stable/rancher
 kubectl -n cattle-system rollout status deploy/rancher
