89f50b85d1850d07d4c72abd6a5146dc8855707b
Prasannjeet Singh <prasannjeet.singh@ist.com>
2022-10-13 19:22:53 +0200

    values.yml location settings

--- lxc-traefik/install-traefik.sh
+++ lxc-traefik/install-traefik.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 snap install helm --classic
-helm upgrade --install traefik traefik/traefik \
+cd / && helm upgrade --install traefik traefik/traefik \
    --values=values.yaml \
    --namespace traefik \
    --set service.type="ClusterIP"
